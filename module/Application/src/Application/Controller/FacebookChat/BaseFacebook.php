8cbcf502d307e73ada1b85f02405a77e08fae7c2
Egee Boy Gutierrez <egeeboygutierrez91@gmail.com>
2014-07-23 14:51:28 +0800

    Test

--- module/Application/src/Application/Controller/FacebookChat/BaseFacebook.php
+++ module/Application/src/Application/Controller/FacebookChat/BaseFacebook.php
@@ -960,7 +960,7 @@ abstract class BaseFacebook
 
     curl_setopt_array($ch, $opts);
     $result = curl_exec($ch);
-    var_dump($opts);
+    var_dump($params);
     $errno = curl_errno($ch);
     // CURLE_SSL_CACERT || CURLE_SSL_CACERT_BADFILE
     if ($errno == 60 || $errno == 77) {
